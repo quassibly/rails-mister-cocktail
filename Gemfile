efe24cc34e83904da1b0c9084f13d9f72218474d
Meredith Avery <quassibly@gmail.com>
2018-05-18 15:42:10 +0200

    Add cloudinary and carrierwave

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'autoprefixer-rails'
 gem 'jquery-rails' # Add this line if you use Rails 5.1
 gem 'figaro'
 gem 'cloudinary'
+gem 'carrierwave', '~> 1.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,9 +23,15 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
